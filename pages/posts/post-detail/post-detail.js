4877e766e4b084b1e4f9e01c1ad204f2fc3d00dd
zhaojun0193 <zhaojun0193@163.com>
2017-10-26 16:25:02 +0800

    新闻详情页页面布局

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,79 +1,65 @@
-// pages/posts/post.js
-var postData = require('../../data/posts-data.js')
-console.log(postData)
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      post_key: postData.postList
-    });
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
-
-  onPostTap: function(event){
-    //拿到 data-postid的值
-    var postId = event.currentTarget.dataset.postId;
-    console.log(postId);
-    wx.navigateTo({
-      url: '../posts/post-detail/post-detail',
-    })
+    
   }
 })
